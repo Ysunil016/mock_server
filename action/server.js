c71f6a1a319018dfdf0d03d9636f46cfc12b6473
Sunil Yadav <sunil.yadav@thoughtworks.com>
2021-05-14 13:40:46 +0530

    sy create request based on different apis provided in configuration

--- action/server.js
+++ action/server.js
@@ -1,11 +1,17 @@
+const express = require("../server/express_server")
+
 function start(server_props) {
-    start_server_log(server_props.name)
+    start_server_log(server_props)
+    create_server(server_props)
 }
 
+function create_server(props){
+    express.create_express_server(props)
+}
 
-
-function start_server_log(name){
-    if(name) console.log("Starting "+name+" Sever");
+function start_server_log(props){
+    const {name,port} = props
+    if(name) console.log("Starting "+name+" Sever on "+port);
     else throw "Please Specify Application Name"
 }
 
---
+++ server/express_server.js
@@ -0,0 +1,27 @@
+const express = require("express")
+
+function create_express_server(props) {
+    const { port, apis } = props
+    const app = express()
+    
+    add_router_to_server(app, apis)
+
+    app.listen(port, () => {
+        console.log(`Success! Your application is running on port ${port}.`);
+    });
+}
+
+function add_router_to_server(server, apis) {
+    apis.forEach(api => {
+        create_route(server, api)
+    });
+}
+
+function create_route(server, api) {
+    const { endpoint, response, status } = api
+    server.get(endpoint, (req, res) => {
+        res.json(response).status(status)
+    })
+}
+
+module.exports = { create_express_server }
