0e617083e08c303f97f513db8a40550526bab1f9
Sunil Yadav <sunil.yadav@thoughtworks.com>
2021-05-15 17:02:47 +0530

    sy add timeout for restart start server

--- action/server.js
+++ action/server.js
@@ -2,12 +2,12 @@ const express = require("../server/express_server");
 const fetch_mock_data = require("../action/mock_data");
 var servers = [];
 async function start(server_props) {
-    server_props.forEach(async(server) => {
+    server_props.forEach(async (server) => {
         await mock_server(server)
     })
 };
 const mock_server = (server_props) => {
-    return new Promise(async(resolve) => {
+    return new Promise(async (resolve) => {
         start_server_log("Mock Server", server_props);
         const server = await create_server(server_props);
         servers.push(server);
@@ -15,27 +15,29 @@ const mock_server = (server_props) => {
     })
 };
 const create_server = (props) => {
-    return new Promise(async(resolve) => {
+    return new Promise(async (resolve) => {
         const created_server = await express.create_express_server(props);
         resolve(created_server)
     })
 };
 const start_server_log = (message, props) => {
-    const {name, port} = props;
+    const { name, port } = props;
     if (name) {
         console.log(message + " on " + port)
     } else {
         throw "Please Specify Correct Server Props"
     }
 };
-const restart = async() => {
-    return new Promise(async(resolve) => {
-        const new_mock_data = await fetch_mock_data();
-        servers.forEach((server, index) => {
-            server.close()
+const restart = async () => {
+    return new Promise(async (resolve) => {
+        servers.forEach(async (server, index) => {
+            await server.close()
         });
         servers = [];
-        await start(new_mock_data);
+        setTimeout(async () => {
+            const new_mock_data = await fetch_mock_data();
+            await start(new_mock_data);
+        }, 30_000)
         resolve(true)
     })
 };
